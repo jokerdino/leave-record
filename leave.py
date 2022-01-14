9d65647945ff573829b19e1530182641f1cba1ae
Barneedhar Vigneshwar <barneedhar@gmail.com>
2022-01-14 22:02:39 +0530

    next version

--- leave.py
+++ leave.py
@@ -1,10 +1,14 @@
 import json
-
+from json.decoder import JSONDecodeError
  
 f = open('data.json')
-d = json.load(f)
+try:
+    d = json.load(f)
+except JSONDecodeError:
+    d = {}
+    pass
 
-print(len(d))
+#print(len(d))
 print(d)
 
 def create_employee():
@@ -13,7 +17,7 @@ def create_employee():
     casualLeave = input("Enter current casual leave: ")
     earnedLeave = input("Enter current earned leave: ")
     sickleave = input("Enter current sick leave: ")
-    d.update({employeeName:{
+    d.update({employeeNumber:{
         "name": employeeName,
         "employee number" : employeeNumber,
         "casual leave": casualLeave,
@@ -23,14 +27,34 @@ def create_employee():
         }
 
 )
-create_employee()
+
+def update_casual_leave():
+    employeeNumber = input("Enter employee number: ")
+    casualLeave = input("Enter new casual leave: ")
+#    print(d.items())
+#    for employeeNumber, casualLeave in d.items():
+    d[employeeNumber]['casual leave'] = casualLeave
+
+    print(d[employeeNumber])
+
+
+options = input("What do you want to do?")
+
+if options == "1":
+    create_employee()
+if options == "2":
+    update_casual_leave()
+if options == "3":
+    employeenumber = input("Enter number to display current status: ")
+    print(d[employeenumber])
+    
 
 a_file = open("data.json", "w")
 json.dump(d,a_file)
 
 a_file.close()
 
-print(d)
+#print(d)
 #a_file = open("data.json", "r")
 #output = a_file.read()
 
